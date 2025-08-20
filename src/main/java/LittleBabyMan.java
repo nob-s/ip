71c5577d68665fc67c1b3e618c6370c09b20ddc8
nob-s <jqlee999@gmail.com>
2025-08-21 01:35:54 +0800

    Update LittleBabyMan.java

---
+++ src/main/java/LittleBabyMan.java
@@ -0,0 +1,13 @@
+public class LittleBabyMan {
+    public static void main(String[] args) {
+        String logo =   " _        ____     __  __ \n" +
+                        "| |      | __ )   |  \\/  |\n" +
+                        "| |      |  _ \\   | |\\/| |\n" +
+                        "| |___   | |_) |  | |  | |\n" +
+                        "|_____|  |____/   |_|  |_|\n";
+        String spacer = "\n_________________________________________________________________________";
+        System.out.println("Hello I'm LittleBabyMan\n" + logo + "\nI'm going to scream. \nAAAAAAAAAAAAAAAAAA" + spacer);
+
+        System.out.println("Bye. I'm going to scream again. \nEEEEEEEEEEEEEEEEEE");
+    }
+}
